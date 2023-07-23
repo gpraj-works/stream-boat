98f83bf85dc086ba4037da3f41b92fc00dd996b1
Kairaa <118424778+Tech-Kairaa@users.noreply.github.com>
2023-07-23 16:29:06 +0530

    test 3

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 const nextConfig = {
 	reactStrictMode: true,
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	httpAgentOptions: {
+		keepAlive: false,
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	  },
+	// distDir: './build',
 };
 
 module.exports = nextConfig;
